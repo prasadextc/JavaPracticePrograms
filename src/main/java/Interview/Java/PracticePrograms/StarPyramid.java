7076018517997d3eb4e9a5e59b0cd9d7c082994c
Prasad Joshi <Prasad.Joshi@ggktech.local>
2019-03-19 12:39:54 +0530

    First Pyramid Program

---
+++ src/main/java/Interview/Java/PracticePrograms/StarPyramid.java
@@ -0,0 +1,31 @@
+package Interview.Java.PracticePrograms;
+
+public class StarPyramid {
+
+	public static void main(String[] args) {
+		int n = 5;
+		printStars(n);
+	}
+	public static void printStars(int n) {
+		int i, j;
+
+		// outer loop to handle number of rows
+		// n in this case
+		for (i = 0; i < n; i++) {
+
+			// inner loop to handle number of columns
+			// values changing acc. to outer loop
+			for (j = 0; j <= i; j++) {
+				// printing stars
+				System.out.print("* ");
+			}
+
+			// ending line after each row
+			System.out.println();
+		}
+	}
+}
+
+	
+
+
